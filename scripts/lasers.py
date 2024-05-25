de626e73f5757303d96051f538c33b0882f0576f
M Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2024-05-25 21:40:55 +0500

    Added some changes

--- scripts/lasers.py
+++ scripts/lasers.py
@@ -12,23 +12,30 @@ class Lasers:
         self.pos_y = pos_y  
 
         def randomSpawn(self):
-            return 
+            self.pos_x = random.randrange((0, self.pos_x))
+ 
         
         
         def randomSize(self):
             self.height = random.randrange((self.height-20), (self.height+20))
             self.width = random.randrange((self.width-20), (self.width+20))
+
+            return [self.witdh, self.height]
+
         
         def attacking(self):
             return 
         
         
         def Yaxis_dec(self):
-            return 
+            self.pos_y +- 1 
         
           
         def randomSpawn(self):
-            return 
+
+
+            width, height = randomSize()
+            rectangle = pygame.rect(randomSpawn, self.pos_y, width, height)
 
         
         
