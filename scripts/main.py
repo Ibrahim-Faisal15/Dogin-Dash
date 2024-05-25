b49d6437d5552127280f875138fdeac9a75d354d
M Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2024-05-25 20:24:28 +0500

    added some chanes

--- scripts/main.py
+++ scripts/main.py
@@ -14,6 +14,8 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
+pos_x = (WIDTH//2)
+
 # Main function
 def main():
     from player import Player  
@@ -24,13 +26,25 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+
+
         screen.fill(WHITE)
         
         # Create a Player object
-        player = Player(GREEN, 20, 0, 0)
+    
+
+        
+        #Control System
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            player.pos_x += player.velocity_x
+            print(player.pos_x)
+
+        player = Player(GREEN, 50, 20, 20, pos_x, (HEIGHT-100))
         player.create_player(screen)
         pygame.display.flip()
-        
+            
+        pygame.display.update()  
         clock.tick(FPS)
 
     pygame.quit()
--- scripts/player.py
+++ scripts/player.py
@@ -2,14 +2,16 @@
 import pygame
 
 class Player:
-    def __init__(self, color,  size, velocity_x, velocity_y):
+    def __init__(self, color,  size, velocity_x, velocity_y, pos_x, pos_y):
         self.color = color
         self.size = size
         self.velocity_x = velocity_x
         self.velocity_y = velocity_y
+        self.pos_x = pos_x
+        self.pos_y = pos_y
         
     def create_player(self, screen):
-        rectangle = pygame.Rect(self.size, self.size, self.size, self.size)
+        rectangle = pygame.Rect(self.pos_x, self.pos_y, self.size, self.size)
         pygame.draw.rect(screen, self.color, rectangle)
 
 
